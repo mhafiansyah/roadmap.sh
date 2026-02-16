1f55e382df7cc9c2ac20b4ed4f411b3d6b10fac1
Miqdad Hafiansyah <mhafiansyah@gmail.com>
2026-02-16 11:25:31 +0700

    first commit

---
+++ backend/02-github-user-activity/index.js
@@ -0,0 +1,30 @@
+import fs from 'node:fs/promises';
+import { URL } from 'node:url';
+
+const username = process.argv[2];
+if (!username) {
+    console.error('Usage: node index.js <username>');
+    process.exit(1);
+}
+
+async function main() {
+    const URL = 'https://jsonplaceholder.typicode.com/users';
+    const options = { headers: { 'User-Agent': 'node-js-cli' } };
+    try {
+        const response = await fetch(URL, options);
+        if (!response.ok) {
+            if (response.ok === 404 ) throw new Error('User not found');
+            throw new Error(`Github API error: ${response.status}`);
+        }
+
+        const results = await response.json();
+
+        results.forEach(event => {
+            console.log(`- ${event.type} in ${event.repo.name}`);
+        });
+    } catch (err) {
+        console.error("🚀 ~ main ~ err:", err)
+    }
+}
+
+main();
