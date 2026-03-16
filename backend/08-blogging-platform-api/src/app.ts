554fb050fe55cd3c7c38f973c3f76a4e17751396
Miqdad Hafiansyah <mhafiansyah@gmail.com>
2026-03-16 17:26:11 +0700

    [08]feat: add rate limit

--- backend/08-blogging-platform-api/src/app.ts
+++ backend/08-blogging-platform-api/src/app.ts
@@ -1,10 +1,11 @@
 import express, { type Request, type Response } from 'express';
 import postsRouter from '@/routes/posts.routes.js';
+import { rateLimiter } from '@/middleware/rateLimit.js';
 
 const app = express();
 
 app.use(express.json());
-app.use('/posts', postsRouter);
+app.use('/posts', rateLimiter, postsRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('welcome');
